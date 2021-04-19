bce2239120d14b86435c61cde9cf543b2e605e5b
snooperv <snooperv@gmail.com>
2021-04-19 20:36:12 +0500

    Add auth

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,8 +37,35 @@
         </ul>
 
         <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Войти</button>
-            <button type="button" class="btn btn-primary">Регистрация</button>
+            @guest
+                @if (Route::has('login'))
+                        <a href="{{ route('login') }}">
+                            <button type="button" class="btn btn-outline-primary me-2">Войти</button></a>
+                @endif
+
+                @if (Route::has('register'))
+                        <a href="{{ route('register') }}">
+                            <button type="button" class="btn btn-primary">Регистрация</button></a>
+                @endif
+            @else
+
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            Выйти
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+
+            @endguest
         </div>
     </header>
 </div>
