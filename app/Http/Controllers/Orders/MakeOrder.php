612464e77907f7d6146b6d691b42223c4de62487
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2021-06-20 20:00:10 +0500

    20.06 MakeOrder: store in process

--- app/Http/Controllers/Orders/MakeOrder.php
+++ app/Http/Controllers/Orders/MakeOrder.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\Orders;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MakeOrder extends Controller
 {
     public function index() {
-
+        //Давать только тех мастеров, что доступны!
+        //И их время!
+        return view('makeOrder');
     }
 
-    public function store() {
+    public function store(Request $request) {
+        Order::create([
+
+        ]);
+        $request->masterName;
+        $request->date;
+        $request->comment;
+        Auth::id();
 
     }
 }
