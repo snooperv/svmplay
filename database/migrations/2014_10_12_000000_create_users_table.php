5cbd7c2d1e4d6108b0015c67e7ad43d7e60365ad
Vadim <snooperv@gmail.com>
2021-06-22 19:16:27 +0500

    Commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
-            $table->foreignId('master_id');
+            $table->string('role')->nullable();
+            $table->foreignId('master_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
