3df2c7d2941aa9a7ec4f81f7ad9e86d1ab37c144
Vadim <snooperv@gmail.com>
2021-06-24 18:33:17 +0500

    Add orders for admin and master 26.06.2021

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -6,22 +6,46 @@
 
 @section('content')
     <div class="orders">
-        {{ $ordersAssignedToMe }}
-    <?php
+        <?php
         use Illuminate\Support\Facades\Auth;
+
+        if (Auth::user()->role == 'MASTER') {
+            echo "<h1>Записи клиентов назначенные на меня</h1>";
+            echo "<table>";
+            echo "<tr>";
+            echo "<td>№</td>";
+            echo "<td>Время</td>";
+            echo "<td>Имя клиента</td>";
+            echo "<td>Комментарий</td>";
+            echo "</tr>";
+
+            foreach (upToArr($ordersAssignedToMe) as $value) {
+                echo "<tr>";
+                echo "<td>" . $value["id"] . "</td>";
+                echo "<td>" . $value["order_time"] . "</td>";
+                echo "<td>" . $value["name"] . "</td>";
+                echo "<td>" . $value["comment"] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+
         if (Auth::user()->role == 'CLIENT' || Auth::user()->role == 'MASTER') {
             echo "<h1>Мои записи</h1>";
-        }
-        else if (Auth::user()->role == 'ADMIN') {
+        } else if (Auth::user()->role == 'ADMIN') {
             echo "<h1>Все записи клиентов</h1>";
         }
         ?>
-        <br>
         <table>
             <tr>
                 <td>№</td>
                 <td>Время</td>
                 <td>Имя мастера</td>
+                <?php
+                if (Auth::user()->role == 'ADMIN') {
+                    echo "<td>Имя клиента</td>";
+                }
+                ?>
                 <td>Комментарий</td>
             </tr>
             <?php
@@ -49,6 +73,7 @@
                     echo "<td>" . $value["id"] . "</td>";
                     echo "<td>" . $value["order_time"] . "</td>";
                     echo "<td>" . $value["master_name"] . "</td>";
+                    echo "<td>" . $value["user_name"] . "</td>";
                     echo "<td>" . $value["comment"] . "</td>";
                     echo "</tr>";
                 }
