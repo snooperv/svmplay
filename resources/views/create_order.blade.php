809aeea5ad99c392778d7429d24f6ed991a7aeac
Vadim <snooperv@gmail.com>
2021-06-24 21:45:33 +0500

    Commit

--- resources/views/create_order.blade.php
+++ resources/views/create_order.blade.php
@@ -11,27 +11,28 @@
                 <div class="card">
                     <div class="card-header">Создать запись</div>
                     <div class="card-body">
-                        <form method="POST" action="#">
+                        <form method="POST" action="make-order">
+                            @csrf
                             <div class="form-group row">
-                                <label for="date" class="col-md-4 col-form-label text-md-right">Время</label>
+                                <label for="order_time" class="col-md-4 col-form-label text-md-right">Время</label>
                                 <div class="col-md-6">
-                                    <input id="date" type="date" class="form-control"
+                                    <input id="order_time" type="date" class="form-control"
                                            required  autofocus>
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="time" class="col-md-4 col-form-label text-md-right">Время</label>
+                                <label for="order_time" class="col-md-4 col-form-label text-md-right">Время</label>
                                 <div class="col-md-6">
-                                    <input id="time" type="time" class="form-control" min="10:00" max="19:00"
+                                    <input id="order_time" type="time" class="form-control" min="10:00" max="19:00"
                                            required>
                                 </div>
                             </div>
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Мастер</label>
+                                <label for="master_id" class="col-md-4 col-form-label text-md-right">Мастер</label>
                                 <div class="col-md-6">
-                                <select class="form-control">
+                                <select id="master_id" class="form-control">
                                     <option>Kirill</option>
                                     <option>Vadim</option>
                                 </select>
@@ -45,7 +46,7 @@
                                 </div>
                             </div>
 
-                            <input type="submit" value="Записаться" class="btn-create-order">
+                            <button type="submit" value="Записаться" class="btn-create-order">Записаться</button>
 
                         </form>
                     </div>
