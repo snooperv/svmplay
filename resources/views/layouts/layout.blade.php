7418970c541b0cb36c43829e7e9313b8e80e6f29
Vadim <snooperv@gmail.com>
2021-06-24 15:48:19 +0500

    Add orders from database 24.06.2021

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <title>NAILS - @yield('title')</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="<?php echo asset('css/nikita.css');?>" rel="stylesheet">
+    <link href="{{ asset('css/nikita.css') }}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,12 +1,41 @@
 @extends('layouts.layout')
 
 @section('title')
-    {{ "Заказы" }}
+    {{ "Мои записи" }}
 @endsection
 
 @section('content')
-    <div class="description">
-        <h1>Заказы</h1>
+    <div class="orders">
+        <h1>Мои записи</h1>
+        <!--{{ $myOrders }} Проверочная переменная-->
+        <br>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td>Время</td>
+                <td>Имя мастера</td>
+                <td>Комментарий</td>
+            </tr>
+            <?php
+            function upToArr($obj)
+            {
+                $orders = array();
+                foreach ($obj as $item => $value) {
+                    $orders = (array)$value;
+                }
+                return $orders;
+            }
+            $myOrders = upToArr($myOrders);
 
+            for ($i = 0; $i < count($myOrders) / 4; $i++) {
+                echo "<tr>";
+                echo "<td>" . $myOrders["id"] . "</td>";
+                echo "<td>" . $myOrders["order_time"] . "</td>";
+                echo "<td>" . $myOrders["master_name"] . "</td>";
+                echo "<td>" . $myOrders["comment"] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
     </div>
 @endsection
